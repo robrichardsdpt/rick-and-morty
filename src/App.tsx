ed7c6265cd0e9facc2b3a79b121c89461e1cc9de
Rob <robrichardsdpt@gmail.com>
2021-02-10 11:49:53 -0500

    updated with v1 time-limited version.

---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import './App.scss'
+// import { data } from './data'
+import axios from 'axios'
+
+interface dataItem {
+  id: number
+  name: string
+  status: string
+  species: string
+  gender: string
+  origin: {
+    name: string
+    url: string
+  }
+  location: {
+    name: string
+    url: string
+  }
+  image: string
+  episode: string[]
+  url: string
+  created: string
+}
+
+const App: React.FC = () => {
+  const [data, setData] = useState<dataItem[]>([])
+
+  useEffect(() => {
+    const random:number = Math.floor(Math.random() * 651)
+    const randomTopRange: number = random + 20
+    const randomArray = (random, randomTopRange) => Array.from({length:RandomTopRange-random},(v, random)=>random+1)
+    console.log(randomArray)
+    axios.get<any>(`https://rickandmortyapi.com/api/character/${random},20`)
+      .then(response => setData(response.data))
+      .catch(err => console.error(err))
+  },[])
+
+  
+  const dataJsx = data.map((item: dataItem) => {
+    return (
+    <div key={item.name} className='data-card'>
+      <div className='image-container'>
+        <img src={item.image} alt={item.name} />
+        <div className='name-container'>
+          <h5>{item.name}</h5>
+        </div>
+      </div>
+      <div className='character-data'>
+        <div className='character-data-item'>
+          <div>STATUS</div>
+          <div>{item.status}</div>
+        </div>
+        <div className='character-data-item'>
+          <div>SPECIES</div>
+          <div>{item.species}</div>
+        </div>
+        <div className='character-data-item'>
+          <div>GENDER</div>
+          <div>{item.gender}</div>
+        </div>
+        <div className='character-data-item'>
+          <div>ORIGIN</div>
+          <div>{item.origin.name}</div>
+        </div>
+        <div className='character-data-item'>
+          <div>LAST LOCATION</div>
+          <div>{item.location.name}</div>
+        </div>
+      </div>
+    </div>
+    )
+  })
+  return (
+    <main>
+      <div className='title'>
+        <h1>Rick and Morty</h1>
+        <div>
+          <h3 className='sub-title'>Hey, did you ever want to hold a Terry fold?</h3>
+        </div>
+      </div>
+      <div className='data-grid'>
+        {dataJsx}
+      </div>
+    </main>
+  )
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const data = [
+  {
+    imageUrl:'images/alan-rails.jpeg',
+    name: 'Alan Rails',
+    status: 'Dead',
+    species: 'Human, Superhuman (Ghost trains summoner)',
+    gender:'Male',
+    origin: 'unknown',
+    lastLocation: "Worldender's lair"
+  },
+  {
+    imageUrl: 'images/bearded-lady.jpeg',
+    name: 'Bearded Lady',
+    status: 'Dead',
+    species: 'Alien, Parasite',
+    gender:'Feale',
+    origin: 'unknown',
+    lastLocation: "Earth (Replacement Dimension)"
+  },
+  {
+    imageUrl: 'images/doofus-rick.jpeg',
+    name: 'Doofus Rick',
+    status: 'unknown',
+    species: 'Human',
+    gender:'Male',
+    origin: 'Earth (J19ζ7)',
+    lastLocation: "Earth (Replacement Dimension)"
+  },
+  {
+    imageUrl: 'images/greasy-grandma.jpeg',
+    name: 'Greasy Grandma',
+    status: 'Alive',
+    species: 'Human, Grandma',
+    gender:'Female',
+    origin: 'Greasy Grandma World',
+    lastLocation: "Greasy Grandma World"
+  },
+  {
+    imageUrl:'images/seal-team-rick.jpeg',
+    name: 'SEAL Team Rick',
+    status: 'Dead',
+    species: 'Human',
+    gender:'Male',
+    origin: 'unknown',
+    lastLocation: "Citadel of Ricks"
+  },
+  {
+    imageUrl: 'images/morphizer-xe-customer-support.jpeg',
+    name: 'Morphizer-XE Customer Support',
+    status: 'unknown',
+    species: 'Alien',
+    gender:'Male',
+    origin: 'unknown',
+    lastLocation: "Earth (Replacement Dimension"
+  }
+]
